476b9d01de76dd8c2c163a710e2ffee07ec771e8
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-03-21 14:59:14 +0300

    Update admin.sql

--- admin.sql
+++ admin.sql
@@ -1,5 +1,5 @@
 CREATE USER administrator WITH PASSWORD 'admin12345';
 
-GRANT SELECT, INSERT ON TABLE clients, banned_clients, speech_therapists;
-GRANT SELECT ON TABLE services, comments, rates, contracts;
+GRANT SELECT, INSERT ON TABLE "clients", "banned_clients", "speech_therapists";
+GRANT SELECT ON TABLE "services", "comments", "rates", "contracts";
   
