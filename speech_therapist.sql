81c72bfb984f4c4880a0a0ac525573326b87344e
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-03-21 15:25:05 +0300

    Update speech_therapist.sql

--- speech_therapist.sql
+++ speech_therapist.sql
@@ -1,3 +1,3 @@
 CREATE USER speech_therapist WITH PASSWORD 'therapist12345';
 
-GRANT SELECT ON TABLE clients, speech_therapists, services, comments, rates TO administrator;
+GRANT SELECT ON TABLE clients, speech_therapists, services, comments, rates TO speech_therapist;
