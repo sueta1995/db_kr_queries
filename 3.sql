f50c7c40938e21326917709c5578adc03e2c8270
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-03-23 16:29:45 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -3,10 +3,17 @@ WITH s_count AS (
 	FROM speech_therapists st, services s
 	WHERE st.id = s.speech_therapist_id
 	GROUP BY st.id
+),
+
+st_rate AS (
+	SELECT r.speech_therapist_id, ROUND(AVG(r.count), 2)
+	FROM rates r
+	GROUP BY r.speech_therapist_id
 )
 
-SELECT c.surname, c.first_name, c.patronymic, c.phone
+SELECT c.surname, c.first_name, c.patronymic, c.phone, str.round
 FROM clients c, speech_therapists st
+JOIN st_rate str ON st.id = str.speech_therapist_id
 WHERE c.id = st.client_id
 AND st.id IN (
 	SELECT MAX(count)
