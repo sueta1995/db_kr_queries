ad9b2e330069b2112b1efadbc09675b58afc3647
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-03-24 20:21:07 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,4 +1,4 @@
-SELECT c.surname, c.first_name, c.patronymic, c.phone, c.birthday 
+SELECT c.surname, c.first_name, c.patronymic, c.birthday, c.phone
 FROM clients c 
 WHERE c.id NOT IN ( 
 	SELECT st.client_id FROM speech_therapists st 
