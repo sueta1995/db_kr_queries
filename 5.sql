3dcedecb1207323dbb2925785074a598a45b3c64
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-03-24 19:54:50 +0300

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -5,11 +5,19 @@ WITH con_clients AS (
 ),
 
 con_max AS (
-	SELECT MAX(conc.sum) DISTINCT
+	SELECT MAX(conc.sum)
 	FROM con_clients conc
+),
+
+c_all AS (
+	SELECT conc.client_id, conc.sum
+	FROM con_clients conc, con_max conm
+	WHERE conc.sum = conm.max
 )
 
-SELECT conc.client_id, conc.sum
-FROM con_clients conc, con_max conm
-JOIN clients c ON conc.client_id = c.id
-WHERE conc.sum = conm.max
+SELECT c.surname, c.first_name, c.patronymic, c.phone
+FROM clients c
+WHERE c.id IN (
+	SELECT client_id
+	FROM c_all
+)
