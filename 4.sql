e06f3617ffba55047a927e80e90bcf22778d7b14
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-04-21 04:11:39 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,19 +1,19 @@
-WITH com_speech_therapists AS (
-	SELECT com.speech_therapist_id, AVG(com.rate_value)
-	FROM comments com
-	GROUP BY com.speech_therapist_id
+WITH con_speech_therapists AS (
+	SELECT con.speech_therapist_id, COUNT(con.id)
+	FROM contracts con
+	GROUP BY con.speech_therapist_id
 ),
 
 st_max AS (
-	SELECT cst.speech_therapist_id, cst.avg
-	FROM com_speech_therapists cst
-	WHERE cst.avg = (
-		SELECT MAX(avg)
-		FROM com_speech_therapists
+	SELECT cst.speech_therapist_id, cst.count
+	FROM con_speech_therapists cst
+	WHERE cst.count = (
+		SELECT MAX(count)
+		FROM con_speech_therapists
 	)
 )
 
-SELECT c.surname, c.first_name, c.patronymic, c.birthday, c.phone, ROUND(stm.avg, 2)
+SELECT c.surname, c.first_name, c.patronymic, c.birthday, c.phone, stm.count
 FROM st_max stm
 JOIN speech_therapists st ON st.id = stm.speech_therapist_id
 JOIN clients c ON c.id = st.client_id
