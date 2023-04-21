f19d8221737b9454d4c4d72c0c0b807b2ed91e59
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-04-21 03:04:18 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,26 +1,19 @@
-WITH con_services AS (
-	SELECT con.service_id, COUNT(con.id)
+WITH con_speech_therapists AS (
+	SELECT con.speech_therapist_id, COUNT(con.id)
 	FROM contracts con
-	GROUP BY con.service_id
-),
-
-st_count AS (
-	SELECT s.speech_therapist_id, SUM(cs.count)
-	FROM con_services cs
-	JOIN services s ON cs.service_id = s.id
-	GROUP BY s.speech_therapist_id
+	GROUP BY con.speech_therapist_id
 ),
 
 st_max AS (
-	SELECT stc.speech_therapist_id, stc.sum
-	FROM st_count stc
-	WHERE stc.sum = (
-		SELECT MAX(sum)
-		FROM st_count
+	SELECT cst.speech_therapist_id, cst.count
+	FROM con_speech_therapists cst
+	WHERE cst.count = (
+		SELECT MAX(count)
+		FROM con_speech_therapists
 	)
 )
 
-SELECT c.surname, c.first_name, c.patronymic, c.birthday, c.phone, stm.sum
+SELECT c.surname, c.first_name, c.patronymic, c.birthday, c.phone, stm.count
 FROM st_max stm
 JOIN speech_therapists st ON st.id = stm.speech_therapist_id
 JOIN clients c ON c.id = st.client_id
