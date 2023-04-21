36e103db48b6ff30f0ae1c287f47bc540e202f6a
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2023-04-21 04:12:12 +0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,15 +1,19 @@
-WITH st_query AS (
-	SELECT st.client_id
-	FROM speech_therapists st
-	WHERE st.id NOT IN (
-		SELECT s.speech_therapist_id
-		FROM services s
+WITH com_speech_therapists AS (
+	SELECT com.speech_therapist_id, AVG(com.rate_value)
+	FROM comments com
+	GROUP BY com.speech_therapist_id
+),
+
+st_max AS (
+	SELECT cst.speech_therapist_id, cst.avg
+	FROM com_speech_therapists cst
+	WHERE cst.avg = (
+		SELECT MAX(avg)
+		FROM com_speech_therapists
 	)
 )
 
-SELECT c.surname, c.first_name, c.patronymic, c.birthday, c.phone
-FROM clients c
-WHERE c.id IN (
-	SELECT client_id
-	FROM st_query
-)
+SELECT c.surname, c.first_name, c.patronymic, c.birthday, c.phone, ROUND(stm.avg, 2)
+FROM st_max stm
+JOIN speech_therapists st ON st.id = stm.speech_therapist_id
+JOIN clients c ON c.id = st.client_id
